de738b81b0a413543b6de30fbfc9d5f7e7cac211
Adir Gelkop <adirgelkop@gmail.com>
2025-04-05 23:28:54 +0300

    server4 is up but now with client4_new and the request-respond is interactive.

---
+++ client4_new.js
@@ -0,0 +1,23 @@
+// A client for the capitalization server. After connecting, every line
+// sent to the server will come back capitalized.
+// Use interactively:
+//
+ 
+const net = require('net');
+const readline = require('readline');
+const client = new net.Socket();
+client.connect(59898, process.argv[2], () => {
+    console.log('Connected to server');
+});
+client.on('data', (data) => {
+    console.log(data.toString('utf-8'));
+});
+const rl = readline.createInterface({
+    input: process.stdin
+});
+rl.on('line', (line) => {
+    client.write(`${line}\n`);
+});
+rl.on('close', () => {
+    client.end();
+});
